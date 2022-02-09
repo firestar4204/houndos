8793e9346d737700592e8e14d82d17c931bad06b
Gabriel Mazzacane <firestar4204@outlook.com>
2022-02-09 11:05:19 -0500

    Use new low-level io functions to make the kernel output X after last output

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,8 +1,16 @@
-void dummy_test_entrypoint()
-{
-}
+#include "../drivers/ports.h"
 
 void kmain(void) {
-	char *video_memory = 0xb8000;
-	*video_memory = 'X';
+	port_byte_out(0x3d4, 14);
+	int position = port_byte_in(0x3d5);
+	position = position << 8;
+
+	port_byte_out(0x3d4, 15);
+	position += port_byte_in(0x3d5);
+
+	int offset_from_vga = position * 2;
+
+	char *vga = 0xb8000;
+	vga[offset_from_vga] = 'X';
+	vga[offset_from_vga + 1] = 0x0f;
 }
